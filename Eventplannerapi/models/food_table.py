e012e5a5759c9fae3a59c17df5b9394401b4cc51
Suganya <suganprakash0621@gmail.com>
2021-03-09 10:33:00 -0600

    new table new data

--- Eventplannerapi/models/food_table.py
+++ Eventplannerapi/models/food_table.py
@@ -4,6 +4,4 @@ class FoodTable(models.Model):
 
     label = models.CharField(max_length=50)
     description = models.CharField(max_length =100)
-    seatingArrangements= models.CharField(max_length =100)
-    recipes = models.CharField(max_length =250)
-    location =models.CharField(max_length =50)
+    foodType = models.ForeignKey("FoodType", on_delete=models.CASCADE)
