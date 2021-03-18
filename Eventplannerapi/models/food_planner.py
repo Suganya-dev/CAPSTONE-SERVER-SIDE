732a38ab90ea4ed41aaf26a4d27ccd2a3d00e2ec
Suganya <suganprakash0621@gmail.com>
2021-03-18 16:14:09 -0500

    working on custom actions

--- Eventplannerapi/models/food_planner.py
+++ Eventplannerapi/models/food_planner.py
@@ -1,9 +1,12 @@
+# File for Model for foodplanner
 from django.db import models
 
 class FoodPlanner(models.Model):
 
+    # related_name defines the property to be joined as a virtual property in the foodplanner model.
+
     events = models.ForeignKey("Events", on_delete=models.CASCADE)
-    foodTable = models.ForeignKey("FoodTable", related_name="foodtable", on_delete=models.CASCADE)
+    foodTable = models.ForeignKey("FoodTable", related_name="foodTable", on_delete=models.CASCADE)
 
 
 
